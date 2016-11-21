f0e175175126d4529094dfa31d09d2f68f54b0e2
YuChi Li <michael520@hotmail.com>
2016-11-20 16:23:13 -0800

    queryDB for data

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,11 +9,10 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
-        [StringLength(255)]
+
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
-        public byte MembershipTpyeId { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
